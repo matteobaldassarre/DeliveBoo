10074f935cad3fcae16b1cdfab8189e715992688
Giovanni-dotcome <giodevito99@gmail.com>
2021-07-12 10:05:36 +0200

    modified type_name

--- database/migrations/2021_07_09_111258_create_types_table.php
+++ database/migrations/2021_07_09_111258_create_types_table.php
@@ -15,7 +15,7 @@ class CreateTypesTable extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
-            $table->string('Type_name', 50); 
+            $table->string('type_name', 50); 
             $table->timestamps();
         });
     }
